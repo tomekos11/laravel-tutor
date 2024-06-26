cd7a89e04a8d7f08ccbf359344748fccf5c7e46f
Tomasz Slapinski <tom.slapinski@gmail.com>
2024-06-26 20:16:25 +0200

    Init

---
+++ frontend/src/layouts/MainLayout.vue
@@ -0,0 +1,15 @@
+<template>
+  <q-layout view="hHh Lpr fFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-toolbar-title> Eraven </q-toolbar-title>
+
+        <div>v1.01</div>
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
---
+++ frontend/src/pages/DesktopPage.vue
@@ -0,0 +1,3 @@
+<template>
+  <div>XDDD</div>
+</template>
